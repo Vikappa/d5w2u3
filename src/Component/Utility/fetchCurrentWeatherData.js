9bcd298905f6f6b2d8adc3324eb8330b13cb71e6
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-02-09 14:03:40 +0100

    Aggiunta logica previsioni locali navbar

--- src/Component/Utility/fetchCurrentWeatherData.js
+++ src/Component/Utility/fetchCurrentWeatherData.js
@@ -1,6 +1,17 @@
-async function fetchCurrentWeatherData () {
+async function fetchCurrentWeatherData(latitudine, longitudine) {
 
+    try {
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitudine}&lon=${longitudine}&units=metric&appid=36f11ef0b08f350df01a926d5e0f7f2d`)
+        if (!response.ok) {
+            throw new Error('Network response was not ok')
+        }
+        const weatherData = await response.json()
+        return weatherData
+    } catch (error) {
+        console.error('Errore fetch weather corrente', error)
+        return null
+    }
 }
 
-
 export default fetchCurrentWeatherData
+
