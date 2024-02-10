4be15e18795477aa3c2c24e43323e8c6c6de2127
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-02-10 01:45:31 +0100

    Update layout responsive

--- src/Component/MainPosition.jsx
+++ src/Component/MainPosition.jsx
@@ -24,13 +24,15 @@ function MainPosition(props) {
                         <div className=' p-1 col-1 d-flex flex-md-column weatherColumnMain'  key={index}>
                             <p className='forecastColumnP text-center mb-0' id='forecastTime'>{dailyForecast.dt_txt}</p>
                             <img className='forecastIcon' alt="dailyIcon" src={weatherImageSorter(dailyForecast.weather[0].main)}></img>
-                           <div className='d-flex flex-md-column justify-content-center align-items-center'>
+                            <div className='d-flex flex-md-column justify-content-center align-items-center'>
                             <p className='forecastColumnP text-center p-0' >{maiuscFirstLetter(dailyForecast.weather[0].description)}</p>
+                            <div className='d-flex flex-column' >
                             <p className='forecastColumnP gradiCol text-center mb-0' >{dailyForecast.main.temp}°</p>
                             <p className='forecastColumnP text-nowrap text-center mb-0' id="minmaxTemp" >(min {dailyForecast.main.temp_min} - max {dailyForecast.main.temp_max})</p>
                             <p className='forecastColumnP text-center' id='tempPerce' >Percepita: {dailyForecast.main.feels_like}</p>
-                            <p className='forecastColumnP text-center' >Umidità: {dailyForecast.main.humidity}%</p>
-                            <p className='forecastColumnP text-center' >Vento: {dailyForecast.wind.speed}mph</p>
+                            </div>
+                            <p className='forecastColumnPHum text-center' >Umidità: {dailyForecast.main.humidity}%</p>
+                            <p className='forecastColumnPSpeed text-center ' >Vento: {dailyForecast.wind.speed}mph</p>
                           </div>
                         </div>
                     )) : <p>Loading...</p>}
