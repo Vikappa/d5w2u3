95c805b1860bc4ce646faffa14c0f46f5968dc87
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-02-11 04:38:28 +0100

    Update leaflet

--- src/Component/MapView.jsx
+++ src/Component/MapView.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import React from 'react'
+import { MapContainer, TileLayer, useMapEvents } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+const MapEvents = () => {
+  useMapEvents({
+    click: (e) => {
+      console.log(`Latitudine: ${e.latlng.lat}, Longitudine: ${e.latlng.lng}`)
+    },
+  })
+  return null
+}
 
 const MapView = () => {
-  const position = [51.505, -0.09]; // Latitudine e longitudine di esempio
+  const position = [41.9027835, 12.4963655] // Latitudine e longitudine di Roma
 
   return (
-    <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>
+    <MapContainer center={position} zoom={10} style={{ height: '400px', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position}>
-        <Popup>
-          Un popup di esempio.
-        </Popup>
-      </Marker>
+      <MapEvents />
     </MapContainer>
   );
 };
 
-export default MapView;
+export default MapView
